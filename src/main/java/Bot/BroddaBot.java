dc3f724450622ef0ba3a5d4e166045c363ac7a66
Elias R <elias.rhoden@hotmail.com>
2018-05-25 22:55:20 +0200

    Provat Kotlin för Theme-song funktionen, funkar localt, skall försöka psuha till heroku

--- src/main/java/Bot/BroddaBot.java
+++ src/main/java/Bot/BroddaBot.java
@@ -1,27 +1,158 @@
 package Bot;
 
 
+
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.handle.obj.IVoiceChannel;
 import sx.blah.discord.util.DiscordException;
 
+import java.util.HashMap;
 import java.util.List;
+
 
 public class BroddaBot {
 
     private IDiscordClient client;
     private EventDispatcher eventDispatcher;
-
+    private HashMap<String, BotAction> botFunctions;
+    private final char BOT_PREFIX = '/';
+    private HashMap<IVoiceChannel,ChannelPlayer> channelPlayers;
+    private JoinWatchK themeSongPlayer;
 
     public BroddaBot(String discordToken){
         this.client = createClient(discordToken);
 
         eventDispatcher = client.getDispatcher();
-        eventDispatcher.registerListener(new MessageListener(client));
+        eventDispatcher.registerListener(new MessageListener(this));
+
+        botFunctions = createBotFunctions();
+        channelPlayers = new HashMap<>();
+
+        themeSongPlayer = new JoinWatchK(this);
+        themeSongPlayer.start();
 
         System.out.println("BRUDDA IS READY!");
+    }
+
+    /**
+     *  Here all bot functions are created and mapped to commands.
+     * */
+
+    // eye of the tiger http://www.math.chalmers.se/~palbin/Eye_of_the_Tiger.mp3
+
+    public HashMap<String, BotAction> createBotFunctions(){
+        HashMap<String, BotAction> functions = new HashMap<>();
+
+        BotAction joinVoice = (event) -> {
+            IVoiceChannel channel = findChannelWithUser(event.getAuthor());
+            channel.join();
+        };
+        functions.put( "joinVoice",joinVoice);
+
+        BotAction playJigga = (event) -> playSong("https://www.youtube.com/watch?v=TXgpwKfmsGg",findChannelWithUser(event.getAuthor()));
+        functions.put( "jiggajigga",playJigga);
+
+        BotAction playPitbull = (event) -> playSong("https://www.youtube.com/watch?v=Rfz6LMVnBVI",findChannelWithUser(event.getAuthor()));
+        functions.put( "pitbull",playPitbull);
+
+        BotAction bruddaTest = (event) -> event.getChannel().sendMessage("DO U KNO DAE WAE?");
+        functions.put( "brudda",bruddaTest);
+
+        BotAction skipToNextSOng= (event) -> nextSong(event);
+        functions.put( "next",skipToNextSOng);
+
+        BotAction playSong = (event) -> {
+            String cmd = event.getMessage().getContent();
+            String[] parts = cmd.split(" ");
+            playSong(parts[1],findChannelWithUser(event.getAuthor()));
+        };
+        functions.put( "play",playSong);
+
+        BotAction stopPlayer= (event) -> stopPlayer(event);
+        functions.put( "stop",stopPlayer);
+
+        BotAction helpFunction = (event) -> event.getChannel().sendMessage("DO U NEED DAE HELP?");
+        functions.put("help",helpFunction);
+
+        return functions;
+    }
+
+    public void excecuteComand(MessageReceivedEvent event) throws Exception{
+        String cmd = event.getMessage().getContent().substring(1);
+        cmd = cmd.split(" ")[0];
+        if(botFunctions.containsKey(cmd)){
+            BotAction action = botFunctions.get(cmd);
+            action.run(event);
+        }else{
+            throw new Exception("Invalid cmd");
+        }
+    }
+
+    public boolean isBotCmd(String msg){
+        char firstChar = msg.charAt(0);
+        return firstChar == BOT_PREFIX;
+    }
+
+    public List<IVoiceChannel> getAllConnectedVoiceChannels(){
+        return client.getVoiceChannels();
+    }
+
+    public ChannelPlayer getVoiceChannelPlayer(IVoiceChannel ch){
+        return channelPlayers.get(ch);
+    }
+
+    private void nextSong(MessageReceivedEvent event) {
+        IVoiceChannel channel = findChannelWithUser(event.getAuthor());
+        if(channelPlayers.containsKey(channel)){
+            channelPlayers.get(channel).next();
+        }
+    }
+
+    private void stopPlayer(MessageReceivedEvent event) {
+        IVoiceChannel channel = findChannelWithUser(event.getAuthor());
+        if(channelPlayers.containsKey(channel)){
+            channelPlayers.get(channel).pause();
+        }
+    }
+
+    public void playSong(String url, IVoiceChannel channel){
+
+        if(!channelPlayers.containsKey(channel)){
+            ChannelPlayer player = new ChannelPlayer(channel.getGuild(),channel);
+            channelPlayers.put(channel,player);
+        }
+        ChannelPlayer player = channelPlayers.get(channel);
+
+        if(player.playerKilled()){
+            channelPlayers.remove(channel);
+            player = new ChannelPlayer(channel.getGuild(),channel);
+            channelPlayers.put(channel,player);
+        }
+
+        channelPlayers.get(channel).playSong(url);
+        channelPlayers.get(channel).resume();
+    }
+
+    public IVoiceChannel findChannelWithUser(IUser u){
+        IVoiceChannel rresult = null;
+
+        for(IVoiceChannel v : client.getVoiceChannels()){
+            System.out.println("LOOKING IN CHANNEL: " + v.getName());
+            for(IUser user:v.getConnectedUsers()){
+                System.out.println("= "+ user.getName());
+                if(user.getName() == u.getName()){
+                    rresult = v;
+                    System.out.println("FOUND CHANNEL!");
+                    break;
+                }
+            }
+        }
+        return rresult;
     }
 
     private IDiscordClient createClient(String token) throws DiscordException {
--- src/main/java/Bot/MessageListener.java
+++ src/main/java/Bot/MessageListener.java
@@ -1,101 +1,15 @@
 package Bot;
 
-import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
-import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.handle.obj.IUser;
-import sx.blah.discord.handle.obj.IVoiceChannel;
-import java.util.HashMap;
+
 
 public class MessageListener implements IListener<MessageReceivedEvent> {
 
-    private IDiscordClient client;
-    private HashMap<String, BotAction> botFunctions;
-    private final char BOT_PREFIX = '/';
-    private HashMap<IChannel,ChannelPlayer> channelPlayers;
+    private BroddaBot bot;
 
-    public MessageListener(IDiscordClient c){
-        client = c;
-        botFunctions = createBotFunctions();
-        channelPlayers = new HashMap<>();
-    }
-
-    /**
-     *  Here all bot functions are created and mapped to commands.
-     * */
-
-    // eye of the tiger http://www.math.chalmers.se/~palbin/Eye_of_the_Tiger.mp3
-
-    public HashMap<String, BotAction> createBotFunctions(){
-        HashMap<String, BotAction> functions = new HashMap<>();
-
-        BotAction joinVoice = (event) -> {
-            IVoiceChannel channel = findChannelWithUser(event.getAuthor());
-            channel.join();
-        };
-        functions.put(BOT_PREFIX + "joinVoice",joinVoice);
-
-        BotAction playJigga = (event) -> playSong("https://www.youtube.com/watch?v=TXgpwKfmsGg",event);
-        functions.put(BOT_PREFIX + "jiggajigga",playJigga);
-
-        BotAction playPitbull = (event) -> playSong("https://www.youtube.com/watch?v=Rfz6LMVnBVI",event);
-        functions.put(BOT_PREFIX + "pitbull",playPitbull);
-
-        BotAction bruddaTest = (event) -> event.getChannel().sendMessage("DO U KNO DAE WAE?");
-        functions.put(BOT_PREFIX + "brudda",bruddaTest);
-
-        BotAction skipToNextSOng= (event) -> nextSong(event);
-        functions.put(BOT_PREFIX + "next",skipToNextSOng);
-
-        BotAction playSong = (event) -> {
-            String cmd = event.getMessage().getContent();
-            String[] parts = cmd.split(" ");
-            playSong(parts[1],event);
-        };
-        functions.put(BOT_PREFIX + "play",playSong);
-
-        BotAction stopPlayer= (event) -> stopPlayer(event);
-        functions.put(BOT_PREFIX + "stop",stopPlayer);
-
-        BotAction helpFunction = (event) -> event.getChannel().sendMessage("DO U NEED DAE HELP?");
-        functions.put(BOT_PREFIX + "help",helpFunction);
-
-        return functions;
-    }
-
-    private void nextSong(MessageReceivedEvent event) {
-        IChannel channel = event.getChannel();
-        if(channelPlayers.containsKey(channel)){
-            channelPlayers.get(channel).next();
-        }
-    }
-
-    private void stopPlayer(MessageReceivedEvent event) {
-        IChannel channel = event.getChannel();
-        if(channelPlayers.containsKey(channel)){
-            channelPlayers.get(channel).pause();
-        }
-    }
-
-    private void playSong(String url, MessageReceivedEvent event){
-        IChannel channel = event.getChannel();
-        IVoiceChannel voiceChannel = findChannelWithUser(event.getAuthor());
-
-        if(!channelPlayers.containsKey(channel)){
-            ChannelPlayer player = new ChannelPlayer(event.getGuild(),voiceChannel);
-            channelPlayers.put(channel,player);
-        }
-        ChannelPlayer player = channelPlayers.get(channel);
-
-        if(player.playerKilled()){
-            channelPlayers.remove(channel);
-            player = new ChannelPlayer(event.getGuild(),voiceChannel);
-            channelPlayers.put(channel,player);
-        }
-
-        channelPlayers.get(channel).playSong(url);
-        channelPlayers.get(channel).resume();
+    public MessageListener(BroddaBot b){
+        bot = b;
     }
 
     @Override
@@ -106,35 +20,16 @@ public class MessageListener implements IListener<MessageReceivedEvent> {
         String msg = event.getMessage().getContent();
         String[] parts = msg.split(" ");
         msg = parts[0];
-
-        if(botFunctions.containsKey(msg)){
-            System.out.println("Valid command: " + msg);
-            BotAction action = botFunctions.get(msg);
-            action.run(event);
-            System.out.println("Action completed!");
-        }else{
-            if(msg.charAt(0) == BOT_PREFIX) {
+        if(bot.isBotCmd(event.getMessage().getContent())) {
+            try {
+                bot.excecuteComand(event);
+            } catch (Exception e) {
                 System.out.println("Invalid command: " + msg);
                 event.getChannel().sendMessage("U DO NOT KNO DAE COMANDS...");
             }
         }
     }
 
-    private IVoiceChannel findChannelWithUser(IUser u){
-        IVoiceChannel rresult = null;
 
-        for(IVoiceChannel v : client.getVoiceChannels()){
-            System.out.println("LOOKING IN CHANNEL: " + v.getName());
-            for(IUser user:v.getConnectedUsers()){
-                System.out.println("= "+ user.getName());
-                if(user.getName() == u.getName()){
-                    rresult = v;
-                    System.out.println("FOUND CHANNEL!");
-                    break;
-                }
-            }
-        }
-        return rresult;
-    }
 
 }
